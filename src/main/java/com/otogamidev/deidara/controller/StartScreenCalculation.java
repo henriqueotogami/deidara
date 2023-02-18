ec6a383f367b2c42cee8d2d501ea95bf074ccbbd
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-02-17 21:13:34 -0300

    refactor: Adjustments to the window structura and styles of the window action buttons.

--- src/main/java/com/otogamidev/deidara/controller/StartScreenCalculation.java
+++ src/main/java/com/otogamidev/deidara/controller/StartScreenCalculation.java
@@ -2,8 +2,6 @@ package com.otogamidev.deidara.controller;
 
 import com.otogamidev.deidara.view.ScreenCalculation;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +9,6 @@ import java.io.IOException;
 public class StartScreenCalculation extends Application {
     @Override
     public void start(final Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(StartScreenCalculation.class.getResource("/hello-view.fxml"));
         final ScreenCalculation screenCalculation = new ScreenCalculation();
         screenCalculation.startScreenCalculation(stage);
     }
