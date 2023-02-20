10adc43519263feb85074e785d2075b414a22658
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-02-20 20:03:43 -0300

    refactor: Adding the back to home screen button.

--- src/main/java/com/otogamidev/deidara/view/LeftBar.java
+++ src/main/java/com/otogamidev/deidara/view/LeftBar.java
@@ -2,6 +2,7 @@ package com.otogamidev.deidara.view;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -32,6 +33,8 @@ public class LeftBar {
         final VBox buttonsVerticalBox = new VBox();
         final VBox groupVerticalBox = new VBox();
 
+        final Button buttonBackToHomePage = createButtonBackToHomePage();
+
         final Button fieldSelectAlgorithmOne = createFieldSelectAlgorithm("AlgorithmOne");
         final Button fieldSelectAlgorithmTwo = createFieldSelectAlgorithm("AlgorithmTwo");
         final Button fieldSelectAlgorithmThree = createFieldSelectAlgorithm("AlgorithmThree");
@@ -46,20 +49,14 @@ public class LeftBar {
         final Image imageDeidara = new Image(pathDeidaraIcon);
         final ImageView iconDeidara = new ImageView();
         final BorderPane paneIconDeidara = new BorderPane();
-        final int paddingTop = 0;
-        final int paddingLeft = 40;
-        final int paddingBottom = 10;
-        final int paddingRight = 40;
-        final Insets positionIconDeidara = new Insets(paddingTop, paddingLeft, paddingBottom, paddingRight);
 
-        iconDeidara.setImage(imageDeidara);
-        iconDeidara.getStyleClass().add("style-deidara-icon");
-        iconDeidara.setFitWidth(120);
-        iconDeidara.setFitHeight(120);
+        final Hyperlink hyperlinkRepository = new Hyperlink("deidara");
 
-        paneIconDeidara.setPrefSize(200,220);
-        paneIconDeidara.setTop(iconDeidara);
-        paneIconDeidara.setPadding(positionIconDeidara);
+        final int paddingImageTop = 0;
+        final int paddingImageLeft = 40;
+        final int paddingImageBottom = 10;
+        final int paddingImageRight = 40;
+        final Insets positionIconDeidara = new Insets(paddingImageTop, paddingImageLeft, paddingImageBottom, paddingImageRight);
 
         buttonsVerticalBox.getChildren().add(fieldSelectAlgorithmOne);
         buttonsVerticalBox.getChildren().add(fieldSelectAlgorithmTwo);
@@ -74,14 +71,46 @@ public class LeftBar {
         buttonsVerticalBox.getStyleClass().add("style-buttons-vertical-box");
         buttonsVerticalBox.getStylesheets().add(pathLocalOpenSans);
 
+        iconDeidara.setImage(imageDeidara);
+        iconDeidara.getStyleClass().add("style-deidara-icon");
+        iconDeidara.setFitWidth(120);
+        iconDeidara.setFitHeight(120);
+
+        paneIconDeidara.setPrefSize(200,220);
+        paneIconDeidara.setTop(iconDeidara);
+        paneIconDeidara.setPadding(positionIconDeidara);
+
+        groupVerticalBox.getChildren().add(buttonBackToHomePage);
         groupVerticalBox.getChildren().add(buttonsVerticalBox);
         groupVerticalBox.getChildren().add(paneIconDeidara);
+        groupVerticalBox.getChildren().add(hyperlinkRepository);
         groupVerticalBox.getStyleClass().add("style-vertical-box");
 
         paneLeftBar.getChildren().add(groupVerticalBox);
         paneLeftBar.getStyleClass().add("style-pane-left-bar");
         paneLeftBar.getStylesheets().add(pathStyleFile);
         return paneLeftBar;
+    }
+
+    private static Button createButtonBackToHomePage() {
+        final int paddingButtonTop = 55;
+        final int paddingButtonLeft = 0;
+        final int paddingButtonBottom = 0;
+        final int paddingButtonRight = 10;
+        final Insets positionButtonBackHome = new Insets(paddingButtonTop, paddingButtonLeft, paddingButtonBottom, paddingButtonRight);
+
+        final Button buttonBackToHomePage = new Button("< BACK");
+
+        buttonBackToHomePage.setMaxWidth(100);
+        buttonBackToHomePage.setMinWidth(100);
+        buttonBackToHomePage.setPrefWidth(100);
+        buttonBackToHomePage.setMaxHeight(20);
+        buttonBackToHomePage.setMinWidth(20);
+        buttonBackToHomePage.setPrefWidth(20);
+        buttonBackToHomePage.setPadding(positionButtonBackHome);
+        buttonBackToHomePage.getStyleClass().add("style-button-back-home-page");
+
+        return buttonBackToHomePage;
     }
 
     private static Button createFieldSelectAlgorithm(final String buttonName) {
@@ -96,6 +125,7 @@ public class LeftBar {
         fieldSelectAlgorithm.setMinHeight(20);
         fieldSelectAlgorithm.setMaxHeight(20);
         fieldSelectAlgorithm.getStyleClass().add("style-field-select-algorithm");
+
         return fieldSelectAlgorithm;
     }
 }
