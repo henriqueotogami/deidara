fe4d75e8d89c1e2e0c7efee0dcca6345282e426d
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-02-22 00:01:08 -0300

    refactor: Adjustments in the project architecture - New application CRC calculation class.

--- src/main/java/com/otogamidev/deidara/models/CalculateCRC16.java
+++ src/main/java/com/otogamidev/deidara/models/CalculateCRC16.java
@@ -1,4 +1,30 @@
-package com.otogamidev.deidara.model;
+package com.otogamidev.deidara.models;
+
+import com.otogamidev.deidara.controllers.SendOutputData;
 
 public class CalculateCRC16 {
+
+    private String dataInputParsedAndReady = null;
+    private String dataOutputCalculated = null;
+
+    private SendOutputData sendOutputData = null;
+    public CalculateCRC16(final String receivedDataParsed) {
+        dataInputParsedAndReady = receivedDataParsed;
+    }
+
+    public String getDataOutputCalculated() {
+        return dataOutputCalculated;
+    }
+
+    public void execute() {
+        System.out.println("CalculateCRC16 - execute(): BEGIN");
+        System.out.println("CalculateCRC16 - execute() - dataInputParsedAndReady: ".concat(dataInputParsedAndReady));
+
+
+
+        System.out.println("CalculateCRC16 - execute() - dataOutputCalculated: ".concat(dataOutputCalculated));
+        System.out.println("CalculateCRC16 - execute(): END");
+    }
+
+
 }
