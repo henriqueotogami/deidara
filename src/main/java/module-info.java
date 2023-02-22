30afa3411dfdab2111823242fa87e3743bf4f6c0
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-02-22 00:04:50 -0300

    refactor: Adjustments in the project architecture - Renaming open packages to JavaFx.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,13 @@ module com.otogamidev.deidara {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens com.otogamidev.deidara.app to javafx.fxml;
+    opens com.otogamidev.deidara.controllers to javafx.fxml;
+    opens com.otogamidev.deidara.models to javafx.fxml;
+    opens com.otogamidev.deidara.views to javafx.fxml;
 
-    opens com.otogamidev.deidara.controller to javafx.fxml;
-    opens com.otogamidev.deidara.view to javafx.fxml;
-
-    exports com.otogamidev.deidara.controller;
-    exports com.otogamidev.deidara.view;
+    exports com.otogamidev.deidara.app;
+    exports com.otogamidev.deidara.controllers;
+    exports com.otogamidev.deidara.models;
+    exports com.otogamidev.deidara.views;
 }
