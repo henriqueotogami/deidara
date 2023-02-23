3451be5299d2dc3b895f2abfc8121faaef37f581
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-02-22 23:20:06 -0300

    doc: Adding JavaDoc to the InvalidData class.

--- src/main/java/com/otogamidev/deidara/exceptions/InvalidData.java
+++ src/main/java/com/otogamidev/deidara/exceptions/InvalidData.java
@@ -1,4 +1,9 @@
 package com.otogamidev.deidara.exceptions;
 
+/**
+ * Classe responsável pelo tratamento de exceções de dados inválidos da aplicação.
+ * @author henriquematheusalvespereira
+ * @since 22-02-2023
+ */
 public class InvalidData {
 }
