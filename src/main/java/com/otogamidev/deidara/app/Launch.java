9e1d146ff972c73fc58209d8fa561b883b36fc23
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-02-22 22:27:30 -0300

    doc: Adding JavaDoc to the Launch class.

--- src/main/java/com/otogamidev/deidara/app/Launch.java
+++ src/main/java/com/otogamidev/deidara/app/Launch.java
@@ -4,14 +4,28 @@ import com.otogamidev.deidara.controllers.MainManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * Classe responsável por inicializar a aplicação, instanciando o gerenciador principal dos controladores.
+ * @author henriquematheusalvespereira
+ * @since 22-02-2023
+ * @see javafx.application.Application
+ */
 public class Launch extends Application {
 
+    /**
+     * Método responsável por inicializar a execução do gerenciador principal dos controladores da aplicação.
+     * @param stage Instância da janela principal
+     */
     @Override
     public void start(final Stage stage) {
         final MainManager manager = new MainManager(stage);
         manager.initExecution();
     }
 
+    /**
+     * Método responsável por invocar a aplicação.
+     * @param args Instância padrão do método main.
+     */
     public static void main(String[] args) {
         launch();
     }
