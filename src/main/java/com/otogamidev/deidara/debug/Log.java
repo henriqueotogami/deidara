36022ce1b1840a4cc622d71e54c107c210d82bd1
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-02-22 23:17:26 -0300

    doc: Adding JavaDoc to the Logs class.

--- src/main/java/com/otogamidev/deidara/debug/Log.java
+++ src/main/java/com/otogamidev/deidara/debug/Log.java
@@ -1,4 +1,9 @@
 package com.otogamidev.deidara.debug;
 
+/**
+ * Classe responsável pela padronização  dos logs de debug no ambiente de desenvolvimento.
+ * @author henriquematheusalvespereira
+ * @since 22-02-2023
+ */
 public class Log {
 }
