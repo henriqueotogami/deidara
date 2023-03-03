fea48ba6b7b6164d5bfadf48cf8780e8221304ee
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-03-03 20:38:01 -0300

    refactor - Adjustments to the parameters passed on to the input controller and their processing.

--- src/main/java/com/otogamidev/deidara/controllers/ReceiveInputData.java
+++ src/main/java/com/otogamidev/deidara/controllers/ReceiveInputData.java
@@ -2,6 +2,8 @@ package com.otogamidev.deidara.controllers;
 
 import com.otogamidev.deidara.models.CalculateCRC16;
 
+import java.util.ArrayList;
+
 /**
  * Classe responsável pela recepção dos dados fornecidos pelo usuário ao gerenciador principal dos controladores.
  * Os dados serão analisados e tratados, para serem enviados para a classe modelo de cálculo de CRC.
@@ -12,6 +14,8 @@ public class ReceiveInputData {
 
 //    Atributos da classe ReceiveInputData
     private String receivedDataToParser = null;
+    private int indexButtonSelected = 0;
+    private ArrayList<Byte> separatedBytesFromReceived = new ArrayList();
     private CalculateCRC16 calculateCRC16 = null;
 
 //    Métodos da classe ReceiveInputData
@@ -19,10 +23,12 @@ public class ReceiveInputData {
     /**
      * Método construtor da classe ReceiveInputData.
      * @param textReceivedInputData Instância contendo os dados a serem analisados e tratados.
+     * @param indexButtonSelected Intância contendo o índice que indica o tipo de dado informado.
      */
-    public ReceiveInputData(final String textReceivedInputData) {
+    public ReceiveInputData(final String textReceivedInputData, final int indexButtonSelected) {
         System.out.println("ReceiveInputData: " + textReceivedInputData);
-        receivedDataToParser = textReceivedInputData;
+        this.receivedDataToParser = textReceivedInputData;
+        this.indexButtonSelected = indexButtonSelected;
     }
 
     /**
@@ -31,7 +37,18 @@ public class ReceiveInputData {
     public void initParserReceivedData(){
         System.out.println("ReceiveInputData - parserReceivedData(): BEGIN");
         if(receivedDataToParser != null){
-            calculateCRC16 = new CalculateCRC16(receivedDataToParser);
+            int indexBegin = 0;
+            int indexEnd = 2;
+            final int sizeToIterateReceivedData = (receivedDataToParser.length()-2);
+            for(int index = 0; (sizeToIterateReceivedData > index); index++) {
+                final String substringByes = receivedDataToParser.substring(indexBegin, indexEnd);
+                final byte convertedBytes = Byte.parseByte(substringByes);
+                separatedBytesFromReceived.add(convertedBytes);
+                indexBegin++;
+                indexEnd++;
+            }
+
+            calculateCRC16 = new CalculateCRC16(separatedBytesFromReceived);
             calculateCRC16.execute();
         }
         System.out.println("ReceiveInputData - parserReceivedData(): END");
