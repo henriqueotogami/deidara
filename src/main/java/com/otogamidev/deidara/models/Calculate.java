351c29e0b9f20d43043690fcbf61b48153e999e8
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-11-22 20:53:02 -0300

    Adicionando as etapas 4 a 6 do descritivo de divisão da mensagem

--- src/main/java/com/otogamidev/deidara/models/Calculate.java
+++ src/main/java/com/otogamidev/deidara/models/Calculate.java
@@ -1,6 +1,5 @@
 package com.otogamidev.deidara.models;
 
-import java.math.BigInteger;
 import java.util.Arrays;
 
 public class Calculate {
@@ -58,73 +57,81 @@ public class Calculate {
         System.out.println("Augmented message  = " + Arrays.toString(augmentedMessage));
         System.out.println("Polynomial         = " + Arrays.toString(polynomial));
 
-        final int[] firstRemainder = new int[polynomial.length];
-        final int[] lastRemainder = new int[polynomial.length];
-        final int[] quotient = new int[augmentedMessageSize];
+        final int[] firstRemainder  = new int[polynomial.length];
+        final int[] lastRemainder   = new int[polynomial.length];
+        final int[] quotient        = new int[augmentedMessageSize];
 
-        boolean makeQuotientXOR = false;
-        boolean makeRemainderXOR = false;
         int messageIndex = 0;
         int countRemainder = 0;
         int displacement = 0;
         int quotientIndex = 0;
         int lastQuotient = 0;
 
+//    1 - Recorto o dividendo da esquerda pra direita, com o mesmo tamanho que o divisor, e salvo como
+//    primeiro resto
+        for (int index = 0; firstRemainder.length > index; index++) {
+            firstRemainder[index] = augmentedMessage[index];
+        }
+
         while(true) {
             System.out.println(" ");
             messageIndex = displacement;
-            final int[] copyOfRange = Arrays.copyOfRange(augmentedMessage, displacement, augmentedMessageRealSize);
-            System.out.println("Augmented message  = " + Arrays.toString(augmentedMessage));
-            System.out.println("Shift message      = " + Arrays.toString(copyOfRange));
-
-            for (int index = 0; firstRemainder.length > index; index++) {
-                firstRemainder[index] = augmentedMessage[messageIndex];
-            }
-            System.out.println("First Remainder " + countRemainder + " = " + Arrays.toString(firstRemainder));
+            System.out.println("Augmented message     = " + Arrays.toString(augmentedMessage));
+            System.out.println("First Remainder " + countRemainder + "     = " + Arrays.toString(firstRemainder));
 
             final String regexRemoveAnyNotRange = "[^a-z0-1]";
             final String nothing = "";
             final String binaryRemainder = Arrays.toString(firstRemainder).replaceAll(regexRemoveAnyNotRange, nothing);
-            System.out.println("binaryRemainder = " + binaryRemainder);
+//            System.out.println("binaryRemainder = " + binaryRemainder);
 
             final int decimalRemainder = Integer.parseInt(binaryRemainder, 2);
-            System.out.println("decimalRemainder = " + decimalRemainder);
+//            System.out.println("decimalRemainder = " + decimalRemainder);
 
             final String binaryDivisor = Arrays.toString(polynomial).replaceAll(regexRemoveAnyNotRange, nothing);
-            System.out.println("binaryDivisor = " + binaryDivisor);
+//            System.out.println("binaryDivisor = " + binaryDivisor);
 
             final int decimalDivisor = Integer.parseInt(binaryDivisor, 2);
-            System.out.println("decimalDivisor = " + decimalDivisor);
+//            System.out.println("decimalDivisor = " + decimalDivisor);
 
+//    2 - Verifico se o valor em decimal do primeiro resto é maior que o quociente
+//            Defino valor 1 para o quociente se o quociente for maior
+//            Defino valor 0 para o quociente se o quociente for menor
             if(decimalDivisor > decimalRemainder) lastQuotient = 1;
             else lastQuotient = 0;
 
             quotient[quotientIndex] = lastQuotient;
+//            System.out.println("lastQuotient = " + lastQuotient);
 
-            System.out.println("lastQuotient = " + lastQuotient);
-
+//    3 - Multiplico o quociente pelo divisor e salvo como o último resto
             for (int index = 0; lastRemainder.length > index; index++) {
                 final int eachBitRemainder = firstRemainder[index];
                 lastRemainder[index] = twoBitsXOR(eachBitRemainder, lastQuotient);
             }
-            System.out.println("Last Remainder " + countRemainder + " = " + Arrays.toString(lastRemainder));
+            System.out.println("Last Remainder " + countRemainder + "      = " + Arrays.toString(lastRemainder));
+
+//    4 - Aplico XOR entre o primeiro e o último resto e salvo o valor no primeiro resto
+            for (int index = 0; lastRemainder.length > index; index++) {
+                final int eachBitFirstRemainder = firstRemainder[index];
+                final int eachBitLastRemainder = lastRemainder[index];
+                firstRemainder[index] = twoBitsXOR(eachBitFirstRemainder, eachBitLastRemainder);
+            }
+            System.out.println("XOR First Remainder " + countRemainder + " = " + Arrays.toString(firstRemainder));
+
+//    5 - Desço o bit mais próximo da direita e salvo no primeiro resto
+            for(int index = 0; firstRemainder.length > index; index++) {
+                if((firstRemainder.length-1) == index)
+                    firstRemainder[index] = augmentedMessage[displacement];
+                else
+                    firstRemainder[index] = firstRemainder[index+1];
+            }
+//            System.out.println("New First Remainder " + countRemainder + " = " + Arrays.toString(firstRemainder));
+
+//    6 - Verifico se chegou no último bit do dividendo
+//        Se sim, retorno o valor do último resto
+//        Se não, continua a partir da etapa 2
+            if(displacement == (originalMessage.length)) break;
 
             quotientIndex += 1;
-
-
-
-            messageIndex = displacement;
-
-            for (int remainderIndex = 0; firstRemainder.length > remainderIndex; remainderIndex++) {
-                final int eachBitRemainder = firstRemainder[remainderIndex];
-                final int eachBitFromMessage = augmentedMessage[messageIndex];
-                firstRemainder[remainderIndex] = twoBitsXOR(eachBitRemainder, eachBitFromMessage);
-                messageIndex += 1;
-            }
-            System.out.println("Second Remainder " + countRemainder + " = " + Arrays.toString(firstRemainder));
-
-            if(messageIndex == (originalMessage.length)) break;
-
             displacement += 1;
             countRemainder += 1;
         }
